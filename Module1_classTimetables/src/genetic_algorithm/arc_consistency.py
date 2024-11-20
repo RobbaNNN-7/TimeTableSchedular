fb7e4ce44a4a6a5d1b52f8d9fca3a9755ee7f8c7
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2024-11-21 01:17:55 +0500

    populated using arc-consistency

--- Module1_classTimetables/src/genetic_algorithm/arc_consistency.py
+++ Module1_classTimetables/src/genetic_algorithm/arc_consistency.py
@@ -1,3 +1,5 @@
+
+
 """
                 FOR ARC CONSISTENCY - TO POPULATE INITIAL TIME-TABLE
 Constraints:
@@ -27,10 +29,63 @@ def arc_consistency(variables,domains,constraints):
                 if Z != X and (Z,X) in constraints:
                     queue.append((Z,X))
 
+    return True
 
-
-def revise(X,Y,domain,constraints):
+def revise(X,Y,domain,constraint):
     revise = False
+    # Remove InConsistencies
+    for value_x in domain[X]:
+        if not any(is_consistent(value_x,value_y,constraint) for value_y in domain[Y]):
+            domain[X].remove(value_x)
+        
+        revise = True
+    
+    return revise
 
-    for x in domain[X]:
-        if is_consistent(x,y) for y in domain[Y]
+# Check if x,y Satisfy the constraint
+def is_consistent(value_x,value_y,constraint):
+    return constraint(value_x,value_y)
+
+def populate_time_table(sections,courses,rooms,instructors,time_slots):
+
+    variables = []
+    domains = {}
+    constraints = {}
+
+    for section in sections:
+        for course in courses:
+            
+            var = f"{course.name} ({section.name})"
+            variables.append(var)
+
+            domains[var] = [(time_slot, room, instructor) 
+                            for time_slot in time_slots
+                            for room in rooms
+                            for instructor in instructors]
+    
+
+    for X in variables:
+        for Y in variables:
+            # Assigning Constraints ,
+            # x[0] -> time_slot
+            # x[1] -> room
+            # x[2] -> instructor
+            # returns True or False for all X,Y
+            constraints[(X,Y)] = lambda x,y: (x[0] != y[0] or x[1] != y[1]) and (x[0] != y[0] or x[2] != y[2])
+
+
+    # applying Arc-Consistency
+    if not arc_consistency(variables,domains,constraints):
+        print("Not Found -(arc)")
+        return # no TimeTable Found 
+    
+    
+    
+
+    
+
+
+    
+
+
+
